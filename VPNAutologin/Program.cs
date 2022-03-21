3191b18c5fbdcb6aba66f0f3db554651c01061c7
Nedavnii Viacheslav <vny@tobii.com>
2022-03-21 13:01:10 +0000

    Extended waiting controls time in case of VPN long thinking
    Kill application in case of error (don't need to close it manually)

--- VPNAutologin/Program.cs
+++ VPNAutologin/Program.cs
@@ -4,6 +4,7 @@ using FlaUI.Core.Input;
 using FlaUI.UIA3;
 using System;
 using System.Threading;
+using FlaUI.Core;
 
 namespace VPNAutologin
 {
@@ -15,13 +16,16 @@ namespace VPNAutologin
             var userName = args[1];
             var userPass = args[2];
 
+            Application application = null;
+
             try
             {
-                using (var app = FlaUI.Core.Application.Launch(appPath))
+                application = Application.Launch(appPath);
                 using (var automation = new UIA3Automation())
                 {
-                    var window = app.GetMainWindow(automation);
-                    var vpnItem = window.FindFirstDescendant(c => c.ByControlType(ControlType.ListItem)).AsListBoxItem();
+                    var window = application.GetMainWindow(automation);
+                    var vpnItem = window.FindFirstDescendant(c => c.ByControlType(ControlType.ListItem))
+                        .AsListBoxItem();
 
                     if (vpnItem.Patterns.SelectionItem.TryGetPattern(out var inv))
                     {
@@ -33,15 +37,17 @@ namespace VPNAutologin
                         Keyboard.Release(FlaUI.Core.WindowsAPI.VirtualKeyShort.KEY_B);
                     }
 
-                    Thread.Sleep(1000);
+                    Thread.Sleep(3000);
 
-                    var login = window.FindFirstDescendant(c => c.ByName("Username:").And(c.ByControlType(ControlType.Edit)));
+                    var login = window.FindFirstDescendant(c =>
+                        c.ByName("Username:").And(c.ByControlType(ControlType.Edit)));
                     if (login.Patterns.Value.TryGetPattern(out var loginText))
                     {
                         loginText.SetValue(userName);
                     }
 
-                    var password = window.FindFirstDescendant(c => c.ByName("Password:").And(c.ByControlType(ControlType.Edit)));
+                    var password = window.FindFirstDescendant(c =>
+                        c.ByName("Password:").And(c.ByControlType(ControlType.Edit)));
                     if (password.Patterns.Value.TryGetPattern(out var passwordText))
                     {
                         passwordText.SetValue(userPass);
@@ -56,8 +62,13 @@ namespace VPNAutologin
             }
             catch (Exception ex)
             {
+                application?.Kill();
                 Console.WriteLine(ex.Message);
                 Console.Read();
+            }
+            finally
+            {
+                application?.Dispose();
             }
         }
     }
